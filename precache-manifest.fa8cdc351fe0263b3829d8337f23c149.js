0e9c6803fe2d2aadc41f5813ab9a1de12a0d4336
Sergio Martínez <sergiotr94@gmail.com>
2022-02-08 13:56:00 +0100

    gh-pages

---
+++ precache-manifest.fa8cdc351fe0263b3829d8337f23c149.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9a7fa1012f7c13f1e8",
+    "url": "/PROJECT_quiz_vue/css/about.c565ed92.css"
+  },
+  {
+    "revision": "7593b6350995df088118",
+    "url": "/PROJECT_quiz_vue/css/app.d5efdf0f.css"
+  },
+  {
+    "revision": "da5a90ca5868d56622f06f61528fe85b",
+    "url": "/PROJECT_quiz_vue/index.html"
+  },
+  {
+    "revision": "ed9a7fa1012f7c13f1e8",
+    "url": "/PROJECT_quiz_vue/js/about.bf2d3209.js"
+  },
+  {
+    "revision": "7593b6350995df088118",
+    "url": "/PROJECT_quiz_vue/js/app.01c24814.js"
+  },
+  {
+    "revision": "a355864f83b134655bb4",
+    "url": "/PROJECT_quiz_vue/js/chunk-vendors.2cd3fd4f.js"
+  },
+  {
+    "revision": "bf11d20cb502933729edbe1a1b0fcaeb",
+    "url": "/PROJECT_quiz_vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/PROJECT_quiz_vue/robots.txt"
+  }
+]);
